a47c33e4c37dad6d857b01e72208b96862f22174
alisilakg <wisper14@yandex.ru>
2023-08-25 20:04:10 +0500

    16 тз : вариант 3

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
