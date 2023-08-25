8f9dfe55340e5fc672647a0d724afad940fd3bb4
alisilakg <wisper14@yandex.ru>
2023-08-25 19:58:57 +0500

    16 тз : вариант 2

--- server/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -6,7 +6,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingInputDto;
-import ru.practicum.shareit.exception.ValidationException;
 
 import java.util.List;
 
